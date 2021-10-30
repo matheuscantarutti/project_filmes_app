24de5e50513fa50324a472bcde7ddbb04f330886
MatheusABC <matheuscantarutti@gmail.com>
2021-10-29 21:12:40 -0300

    ajuste de ícones do menu

--- app/src/main/java/com/example/filmesapp/MainActivity.java
+++ app/src/main/java/com/example/filmesapp/MainActivity.java
@@ -41,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_filmes, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        //NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
